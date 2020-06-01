092e3b991f4e05e37b43082695013cb321865112
Jack Holdsworth <jackch90@gmail.com>
2020-06-01 20:28:45 +0100

    Skeleton for task CRUD actions

---
+++ controllers/taskController.js
@@ -0,0 +1,33 @@
+const Task = require('../models/Task');
+
+exports.task_list = function(req, res) {
+
+}
+
+exports.task_detail = function(req, res) {
+
+}
+
+exports.task_create_get = function(req, res) {
+
+}
+
+exports.task_create_post = function(req, res) {
+
+}
+
+exports.task_update_get = function(req, res) {
+
+}
+
+exports.task_update_post = function(req, res) {
+
+}
+
+exports.task_delete_get = function(req, res) {
+
+}
+
+exports.task_delete_post = function (req, res) {
+
+}
